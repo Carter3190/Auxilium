2629f36b9b0b0c3027429d5a27c5e64731c797c7
Carter3190 <csabes3190@gmail.com>
2025-04-02 17:12:05 -0400

    Update and rename script.js to Script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById("login").addEventListener("click", function() {
-            window.location.href = "login.html";
-        });
-    });
-</script>
